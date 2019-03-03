f28f254150097bf7379300ddc123d0907e6fd3b0
luckypro2 <48180833+luckypro2@users.noreply.github.com>
2019-03-03 17:34:48 +0300

    hw2_A2

---
+++ hw2_A2.py
@@ -0,0 +1,20 @@
+def f():
+    for i in range(10):
+         print("Hello World!") 
+
+f() #Вариант 1
+
+print("Hello World!" * 10) #Вариант 2
+
+
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!") #Вариант 3
+
