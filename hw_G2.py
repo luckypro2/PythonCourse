2798a145af4dffdd04c5580bf0dadfc16fbdcbed
luckypro2 <48180833+luckypro2@users.noreply.github.com>
2019-04-19 20:26:41 +0300

    Add files via upload

---
+++ hw_G2.py
@@ -0,0 +1,9 @@
+#Fermat's little theorem states that if p is a prime number, then for any integer a, the number ap − a is an integer multiple of p.
+
+def primes(end):
+    for p in range(2, end):
+        if (2**(p)-2)% p == 0:
+            yield p
+
+for number in primes(35000):
+    print(number, end = ' ')
